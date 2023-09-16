fa91244814d3d44730f502e8f6cef9be408454da
Rashmika Pramodya <rashmikapramodya@gmail.com>
2023-09-16 11:19:09 +0530

    Recreated the DB

---
+++ payment.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+    <link rel="stylesheet" href="payment.css">
+</head>
+<body>
+
+<div class="container">
+
+    <form action="">
+
+        <div class="row">
+
+            <!--div class="col">
+
+                <h3 class="title">billing address</h3>
+
+                <div class="inputBox">
+                    <span>full name :</span>
+                    <input type="text">
+                </div>
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email">
+                </div>
+                <div class="inputBox">
+                    <span>address :</span>
+                    <input type="text">
+                </div>
+                <div class="inputBox">
+                    <span>city :</span>
+                    <input type="text">
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                        <span>state :</span>
+                        <input type="text">
+                    </div>
+                    <div class="inputBox">
+                        <span>zip code :</span>
+                        <input type="text">
+                    </div>
+                </div>
+
+            </div-->
+
+            <div class="col">
+
+                <h3 class="title">payment</h3>
+
+                <div class="inputBox">
+                    <span>cards accepted :</span>
+                    <img src="images/card_img.png" alt="">
+                </div>
+                <div class="inputBox">
+                    <span>name on card :</span>
+                    <input type="text" >
+                </div>
+                <div class="inputBox">
+                    <span>credit card number :</span>
+                    <input type="number">
+                </div>
+                <div class="inputBox">
+                    <span>exp month :</span>
+                    <input type="text">
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                        <span>exp year :</span>
+                        <input type="number">
+                    </div>
+                    <div class="inputBox">
+                        <span>CVV :</span>
+                        <input type="text">
+                    </div>
+                </div>
+
+            </div>
+    
+        </div>
+
+        <input type="submit" value="Checkout" class="submit-btn">
+
+    </form>
+
+</div>    
+    
+</body>
+</html>
